51898bfd88c79d07cfacc289db310dfb61d9e516
Ashu1631 <ashismehara.cozy@gmail.com>
2026-03-30 12:15:59 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+const historyData = [
+
+{
+  id:"temple",
+  title:"Indian Temple History",
+  image:"https://upload.wikimedia.org/wikipedia/commons/3/3f/Brihadeeswarar_Temple.jpg",
+  points:[
+    "Temples small se bade bane",
+    "3 styles: Nagara, Dravida, Vesara",
+    "India me hazaaro temples hain"
+  ]
+},
+
+{
+  id:"egypt",
+  title:"Egypt Civilization",
+  image:"https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg",
+  points:[
+    "Pyramids famous",
+    "Pharaoh king",
+    "Mummies banate the"
+  ]
+}
+
+];
