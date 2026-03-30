029be2350d3d062ddbf3198a653dec1b8ac07606
Ashu1631 <ashismehara.cozy@gmail.com>
2026-03-30 12:18:13 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,189 @@
 const historyData = [
 
+/* 🏛 INDIAN TEMPLE HISTORY */
 {
   id:"temple",
   title:"Indian Temple History",
   image:"https://upload.wikimedia.org/wikipedia/commons/3/3f/Brihadeeswarar_Temple.jpg",
   points:[
-    "Temples small se bade bane",
-    "3 styles: Nagara, Dravida, Vesara",
-    "India me hazaaro temples hain"
+    "Temples pehle small sacred places the",
+    "Baad me bade aur beautiful ban gaye",
+    "3 styles: Nagara (North), Dravida (South), Vesara (Mixed)",
+    "India me hazaaro temples hain",
+    "Architecture aur culture ka symbol hain"
   ]
 },
 
+{
+  id:"kailash",
+  title:"Kailash Temple",
+  image:"https://upload.wikimedia.org/wikipedia/commons/1/1e/Kailasa_temple_ellora.jpg",
+  points:[
+    "Ek hi rock se carve kiya gaya",
+    "Ellora caves me hai",
+    "Rashtrakuta kings ne banwaya",
+    "Shiv ji ko dedicate hai"
+  ]
+},
+
+{
+  id:"konark",
+  title:"Konark Sun Temple",
+  image:"https://upload.wikimedia.org/wikipedia/commons/4/4a/Konark_Sun_Temple.jpg",
+  points:[
+    "Rath (chariot) shape ka temple",
+    "Sun God ko dedicate hai",
+    "Odisha me located hai",
+    "UNESCO World Heritage site"
+  ]
+},
+
+{
+  id:"meenakshi",
+  title:"Meenakshi Temple",
+  image:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Meenakshi_Amman_Temple.jpg",
+  points:[
+    "Madurai me hai",
+    "Colorful towers (gopuram)",
+    "South India ka famous temple",
+    "Tourists aur devotees dono aate hain"
+  ]
+},
+
+{
+  id:"golden",
+  title:"Golden Temple",
+  image:"https://upload.wikimedia.org/wikipedia/commons/6/6d/Golden_Temple.jpg",
+  points:[
+    "Amritsar me hai",
+    "Sikh religion ka main temple",
+    "Gold se covered hai",
+    "Langar (free food) milta hai"
+  ]
+},
+
+{
+  id:"chardham",
+  title:"Char Dham",
+  image:"https://upload.wikimedia.org/wikipedia/commons/5/5d/Badrinath_Temple.jpg",
+  points:[
+    "4 important temples India ke",
+    "Badrinath (North)",
+    "Dwarka (West)",
+    "Puri (East)",
+    "Rameswaram (South)"
+  ]
+},
+
+/* 🏺 EGYPT */
 {
   id:"egypt",
   title:"Egypt Civilization",
   image:"https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg",
   points:[
-    "Pyramids famous",
-    "Pharaoh king",
-    "Mummies banate the"
+    "Nile river ke paas develop hua",
+    "Pharaoh king hota tha",
+    "Pyramids banaye gaye",
+    "Mummies banayi jati thi",
+    "Hieroglyphics writing system tha"
+  ]
+},
+
+/* 🏺 MESOPOTAMIA */
+{
+  id:"mesopotamia",
+  title:"Mesopotamia Civilization",
+  image:"https://upload.wikimedia.org/wikipedia/commons/0/0f/Ziggurat_of_Ur.jpg",
+  points:[
+    "World ki first civilization mani jati hai",
+    "Tigris aur Euphrates rivers ke beech",
+    "Cuneiform writing system",
+    "Hammurabi law system famous tha",
+    "Cities jaise Ur aur Babylon famous the"
+  ]
+},
+
+/* 🇮🇳 INDIA HISTORY */
+{
+  id:"india",
+  title:"India History",
+  image:"https://upload.wikimedia.org/wikipedia/commons/1/1d/Mohenjo-daro.jpg",
+  points:[
+    "Indus Valley civilization planned cities",
+    "Maurya Empire (Ashoka)",
+    "Gupta Empire Golden Age",
+    "Mughal Empire powerful tha",
+    "1947 me independence mili"
+  ]
+},
+
+/* 🇨🇳 CHINA */
+{
+  id:"china",
+  title:"China History",
+  image:"https://upload.wikimedia.org/wikipedia/commons/6/6f/GreatWall_2004_Summer_4.jpg",
+  points:[
+    "Ancient dynasties: Xia, Shang, Zhou",
+    "Great Wall famous hai",
+    "Silk Road trade route tha",
+    "Paper aur compass invent kiya",
+    "1949 me modern China bana"
+  ]
+},
+
+/* 🇬🇷 GREECE */
+{
+  id:"greece",
+  title:"Greece Civilization",
+  image:"https://upload.wikimedia.org/wikipedia/commons/d/da/Acropolis_of_Athens.jpg",
+  points:[
+    "Athens aur Sparta famous cities",
+    "Democracy yahin se start hua",
+    "Philosophers jaise Socrates",
+    "Olympics yahin start hua",
+    "Alexander the Great famous ruler"
+  ]
+},
+
+/* 🇮🇹 ROME */
+{
+  id:"rome",
+  title:"Roman Empire",
+  image:"https://upload.wikimedia.org/wikipedia/commons/6/6f/Colosseum_in_Rome.jpg",
+  points:[
+    "Roman Empire bahut powerful tha",
+    "Law system develop hua",
+    "Colosseum famous building",
+    "Christianity yahan spread hui",
+    "476 CE me fall hua"
+  ]
+},
+
+/* 💣 WORLD WARS */
+{
+  id:"ww",
+  title:"World Wars",
+  image:"https://upload.wikimedia.org/wikipedia/commons/a/a3/WW2_collage.png",
+  points:[
+    "World War 1 (1914-1918)",
+    "World War 2 (1939-1945)",
+    "Millions log mare",
+    "Atomic bomb Japan par gira",
+    "World politics change ho gaya"
+  ]
+},
+
+/* 🇫🇷 FRENCH REVOLUTION */
+{
+  id:"france",
+  title:"French Revolution",
+  image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/French_Revolution.jpg",
+  points:[
+    "1789 me start hua",
+    "Log king ke khilaf khade hue",
+    "Liberty, Equality, Fraternity slogan",
+    "King Louis XVI ko hata diya",
+    "Democracy ka start hua"
   ]
 }
 
